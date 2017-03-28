4bd57da324f5b79a6ea610811087953a6d12baca
scott0301 <gabester@daum.net>
2017-03-28 14:51:36 +0900

    Initial Test

---
+++ UC_MessageWindow/UC_MessageWindow.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace UC_MessageWindow
+{
+    partial class UC_BACKPANEL
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RICH_MESSAGE_WINDOW = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // RICH_MESSAGE_WINDOW
+            // 
+            this.RICH_MESSAGE_WINDOW.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.RICH_MESSAGE_WINDOW.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold);
+            this.RICH_MESSAGE_WINDOW.ForeColor = System.Drawing.Color.Lime;
+            this.RICH_MESSAGE_WINDOW.Location = new System.Drawing.Point(3, 3);
+            this.RICH_MESSAGE_WINDOW.Name = "RICH_MESSAGE_WINDOW";
+            this.RICH_MESSAGE_WINDOW.Size = new System.Drawing.Size(535, 237);
+            this.RICH_MESSAGE_WINDOW.TabIndex = 0;
+            this.RICH_MESSAGE_WINDOW.Text = "";
+            // 
+            // UC_BACKPANEL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.RICH_MESSAGE_WINDOW);
+            this.Font = new System.Drawing.Font("Verdana", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Strikeout))));
+            this.Name = "UC_BACKPANEL";
+            this.Size = new System.Drawing.Size(543, 245);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RICH_MESSAGE_WINDOW;
+    }
+}
---
+++ UC_MessageWindow/UC_MessageWindow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UC_MessageWindow
+{
+    public partial class UC_BACKPANEL: UserControl
+    {
+        public UC_BACKPANEL()
+        {
+            InitializeComponent();
+        }
+
+        public void InsertMessage(string msg)
+        {
+            string strTime = string.Format("[{0:00}-{1:00}|{2:00}:{3:00}:{4:00}:{5:000}] ", DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second, DateTime.Now.Millisecond);
+
+            this.Invoke(new MethodInvoker(delegate
+            {
+                RICH_MESSAGE_WINDOW.AppendText(strTime + msg + System.Environment.NewLine);
+                RICH_MESSAGE_WINDOW.ScrollToCaret();
+            }));
+        }
+
+        public void Clear()
+        {
+            RICH_MESSAGE_WINDOW.Clear();
+        }
+
+        public void SetSize(int w, int h)
+        {
+            this.Size = new Size(w, h);
+            RICH_MESSAGE_WINDOW.Size = new Size(w, h);
+        }
+
+    }
+}
